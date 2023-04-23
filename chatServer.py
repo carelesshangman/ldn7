f98bece2ca99f6be812f1376b185a2086d0888ee
Jaka German <89906015+carelesshangman@users.noreply.github.com>
2023-04-23 23:09:25 +0200

    Update chatServer.py

--- chatServer.py
+++ chatServer.py
@@ -1,9 +1,9 @@
 import signal
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 import socket
 import struct
 import threading
-
-signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 PORT = 1234
 HEADER_LENGTH = 2
@@ -12,96 +12,108 @@ HEADER_LENGTH = 2
 def receive_fixed_length_msg(sock, msglen):
     message = b''
     while len(message) < msglen:
-        chunk = sock.recv(msglen - len(message))  # read some bytes
+        chunk = sock.recv(msglen - len(message))  # preberi nekaj bajtov
         if chunk == b'':
             raise RuntimeError("socket connection broken")
-        message = message + chunk  # attach read bytes to the message
+        message = message + chunk  # pripni prebrane bajte sporocilu
 
     return message
 
 
 def receive_message(sock):
     header = receive_fixed_length_msg(sock,
-                                      HEADER_LENGTH)  # read message header (the first 2 bytes contain the message length)
-    message_length = struct.unpack("!H", header)[0]  # convert message length to int
+                                      HEADER_LENGTH)  # preberi glavo sporocila (v prvih 2 bytih je dolzina sporocila)
+    message_length = struct.unpack("!H", header)[0]  # pretvori dolzino sporocila v int
 
     message = None
-    if message_length > 0:  # if everything is OK
-        message = receive_fixed_length_msg(sock, message_length)  # read the message
+    if message_length > 0:  # ce je vse OK
+        message = receive_fixed_length_msg(sock, message_length)  # preberi sporocilo
         message = message.decode("utf-8")
 
     return message
 
 
 def send_message(sock, message):
-    encoded_message = message.encode("utf-8")  # convert the message to a byte string, use the UTF-8 code table
+    encoded_message = message.encode("utf-8")  # pretvori sporocilo v niz bajtov, uporabi UTF-8 kodno tabelo
 
-    header = struct.pack("!H", len(encoded_message))  # create the header, the first 2 bytes contain the message length (HEADER_LENGTH)
-                                                      # pack method "!H" : !=network byte order, H=unsigned short
-    message = header + encoded_message  # first send the message length, then the message itself
+    # ustvari glavo v prvih 2 bytih je dolzina sporocila (HEADER_LENGTH)
+    # metoda pack "!H" : !=network byte order, H=unsigned short
+    header = struct.pack("!H", len(encoded_message))
+
+    message = header + encoded_message  # najprj posljemo dolzino sporocilo, slee nato sporocilo samo
     sock.sendall(message)
 
 
+usernames = []
+# funkcija za komunikacijo z odjemalcem (tece v loceni niti za vsakega odjemalca)
 def client_thread(client_sock, client_addr):
     global clients
-
-    print("[system] connected with " + client_addr[0] + ":" + str(client_addr[1]))
+    global usernames
+    # Prompt user to enter a username upon connection
+    send_message(client_sock, "Enter your username:")
+    username = receive_message(client_sock)
+    usernames.append((username, client_sock))
+    print("[system] connected with " + client_addr[0] + ":" + str(client_addr[1]) + " as " + username)
     print("[system] we now have " + str(len(clients)) + " clients")
 
-    username = receive_message(client_sock)
-    if username is None:
-        return
-
-    with clients_lock:
-        clients[username.lower()] = client_sock
-
     try:
-        while True:
+
+        while True:  # neskoncna zanka
             msg_received = receive_message(client_sock)
 
-            if not msg_received:
+            if not msg_received:  # ce obstaja sporocilo
                 break
 
-            if msg_received.startswith("/whisper"):
-                parts = msg_received.split(" ", 2)
-                if len(parts) < 3:
-                    continue
+            if (msg_received.split(" ")[1] == '/whisper' or msg_received.split(" ")[1] == '/w') and len(msg_received.split(" ")) >= 4:
+                timestamp = msg_received.split(" ")[0]
+                sendTo = msg_received.split(" ")[2]
+                textBlob = msg_received.split(" ")[3:]
+                flagRecipient = False
+                for sendToUsername, client_socket in usernames:
+                    if sendToUsername == sendTo:
+                        flagRecipient = True
+                        result = ' '.join(textBlob)
+                        send_message(client_socket, f"{timestamp} [{username}]->[{sendToUsername}]: {result}")
+                        send_message(client_sock, f"{timestamp} [{username}]->[{sendToUsername}]: {result}")
+                if flagRecipient == False:
+                    send_message(client_sock, f"{timestamp} [System]: User '{sendTo}' not found.")
+                continue
 
-                target_username, private_message = parts[1], parts[2]
-                target_socket = clients.get(target_username.lower())
+            # Prefix message with username
+            msg_with_username = "[" + username + "] " + msg_received
 
-                if target_socket:
-                    send_message(target_socket, f"[Private] {username}: {private_message}")
-                else:
-                    send_message(client_sock, f"[Error] User '{target_username}' not found.")
-            else:
-                broadcast_message = f"[RKchat] [{username}] : {msg_received}"
-                print(broadcast_message)
+            print("[RKchat] " + msg_with_username)
 
-                with clients_lock:
-                    for target_socket in clients.values():
-                        if target_socket != client_sock:
-                            send_message(target_socket, broadcast_message)
+            for client in clients:
+                send_message(client, msg_with_username.upper())
     except:
+        # tule bi lahko bolj elegantno reagirali, npr. na posamezne izjeme. Trenutno kar pozremo izjemo
         pass
 
+    # prisli smo iz neskoncne zanke
     with clients_lock:
-        del clients[username.lower()]
+        clients.remove(client_sock)
     print("[system] we now have " + str(len(clients)) + " clients")
     client_sock.close()
 
 
+# kreiraj socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("localhost", PORT))
 server_socket.listen(1)
 
+# cakaj na nove odjemalce
 print("[system] listening ...")
-clients = {}
+clients = set()
 clients_lock = threading.Lock()
 while True:
     try:
+        # pocakaj na novo povezavo - blokirajoc klic
         client_sock, client_addr = server_socket.accept()
-        thread = threading.Thread(target=client_thread, args=(client_sock, client_addr))
+        with clients_lock:
+            clients.add(client_sock)
+
+        thread = threading.Thread(target=client_thread, args=(client_sock, client_addr));
         thread.daemon = True
         thread.start()
 
